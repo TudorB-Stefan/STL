66f1f7e434f476394fd778f54449df65f1abd2eb
TudorB-Stefan <tudor.boana@gmail.com>
2024-11-02 10:14:58 +0200

    STL-homewrk-1

--- main.cpp
+++ main.cpp
@@ -1,6 +1,7 @@
 #include <iostream>
 #include <fstream>
 #include <string>
+#include <vector>
 
 using namespace std;
 
@@ -10,14 +11,19 @@ int main()
 
     int no_problems, no_doctors;
     string name, speciality;
-    
+
+    vector<string> doctors;
+    vector<string> namePacient;
+    vector<string> pacients;
+
     inFile >> no_problems;
 
     for (int i = 0; i < no_problems; i++)
     {
         inFile >> name;
+        namePacient.push_back(name);
         inFile >> speciality;
-        cout << name << ' ' << speciality << '\n';
+        pacients.push_back(speciality);
     }
 
     inFile >> no_doctors;
@@ -26,8 +32,18 @@ int main()
     {
         inFile >> name;
         inFile >> speciality;
-        cout << name << ' ' << speciality << '\n';
+        doctors.push_back(speciality);
     }
-
+    for(auto i = 0;i<pacients.size() ;i++) {
+        int ok=1;
+        for(auto j = 0;j<doctors.size() ;j++) {
+            if(pacients[i] == doctors[j]) {
+                cout<<namePacient[i]<<" ADMIS"<<endl;
+                ok=0;
+                break;
+            }
+        }
+        if(ok) cout<<namePacient[i]<<" RESPINS"<<endl;
+    }
     return 0;
 }
